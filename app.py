3f8da5bfe6527877c7739ddd54711d6913d417b7
20ramachandrana <44482134+20ramachandrana@users.noreply.github.com>
2021-04-22 18:21:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,13 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template("home.html", message = "Texas Grid Projection Calculator!")
-
+@app.route('/userguide')
+def userguide():
+    return render_template("userguide.html")
 @app.route('/calculator')
 def calc():
     return render_template("calc.html")
+
 
 if __name__ == "__main__":
     app.run()
