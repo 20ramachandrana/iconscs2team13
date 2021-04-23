74ec5d8d65d0590016d42677396d74ca110ced5f
20ramachandrana <44482134+20ramachandrana@users.noreply.github.com>
2021-04-23 10:54:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,11 @@ def calc():
         return render_template("calc.html")
     else:
         return render_template("data.html")
-
+@app.route('/calculatorver2', methods = ['GET', 'POST'])
+def calc():
+    if request.method == 'GET':
+        return render_template("calcver2.html")
+    else:
+        return render_template("data.html")
 if __name__ == "__main__":
     app.run(debug=True)
