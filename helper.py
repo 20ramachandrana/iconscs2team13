e5bfe7b4c314a95faf150802443ea89347bd4a8b
20ramachandrana <44482134+20ramachandrana@users.noreply.github.com>
2021-04-24 18:25:03 -0400

    Update helper.py

--- helper.py
+++ helper.py
@@ -167,7 +167,8 @@ def getPlots(startYear, endYear, initialSrc, firsDerSrc, secDerSrc, percEff):
     # Saving Plot 1 - Emissions image
     energy = get_aEnergy(years)
     plt.plot(years, energy)
-    plt.xticks(list(range(startYear, endYear+1, 1)))
+    if endYear-StartYear < 10:
+        plt.xticks(list(range(startYear, endYear+1, 1)))
     plt.xlabel("Years")
     plt.ylabel("Energy Production MWhr")
     plt.title("Annual Energy Production")
@@ -182,7 +183,8 @@ def getPlots(startYear, endYear, initialSrc, firsDerSrc, secDerSrc, percEff):
     # Saving Plot 2 - CO2 Emissions image
     emissions = get_aEmissions(years, percSources, energy)
     plt.plot(years, emissions)
-    plt.xticks(list(range(startYear, endYear+1, 1)))
+    if endYear-StartYear < 10:
+        plt.xticks(list(range(startYear, endYear+1, 1)))
     plt.xlabel("Years")
     plt.ylabel("CO2 Emissions (Million Metric Tons)")
     plt.title("Annual Carbon Emissions")
@@ -202,7 +204,8 @@ def getPlots(startYear, endYear, initialSrc, firsDerSrc, secDerSrc, percEff):
     PercNuclear = percSources[:,4]
     PercWinterNG = percSources[:,5]
     plt.plot(years, PercCoal, "k", years, PercNG, "r", years, PercSolar, "y", years, PercWind, "b", years, PercNuclear, "g", years, PercWinterNG, "m")
-    plt.xticks(list(range(startYear, endYear+1, 1)))
+    if endYear-StartYear < 10:
+        plt.xticks(list(range(startYear, endYear+1, 1)))
     plt.xlabel("Years")
     plt.ylabel("% Source By Energy Production")
     plt.title("Percent Distribution of Sources Over Time")
@@ -218,7 +221,8 @@ def getPlots(startYear, endYear, initialSrc, firsDerSrc, secDerSrc, percEff):
     # Saving Plot 4 - Reliability Image
     reliability = get_aReliability(years, percSources, energy, percEff)
     plt.plot(years, reliability)
-    plt.xticks(list(range(startYear, endYear+1, 1)))
+    if endYear-StartYear < 10:
+        plt.xticks(list(range(startYear, endYear+1, 1)))
     plt.xlabel("Years")
     plt.ylabel("Reliability (expected reduction in MWhr)")
     plt.title("Reliability Over Time")
@@ -233,7 +237,8 @@ def getPlots(startYear, endYear, initialSrc, firsDerSrc, secDerSrc, percEff):
     # Saving Plot 5 - Construction Cost
     construcCost = get_aConstructionCost(years, percSources)
     plt.plot(years, construcCost)
-    plt.xticks(list(range(startYear, endYear+1, 1)))
+    if endYear-StartYear < 10:
+        plt.xticks(list(range(startYear, endYear+1, 1)))
     plt.xlabel("Years")
     plt.ylabel("Cost (USD)")
     plt.title("Annual Construction Costs")
@@ -248,7 +253,8 @@ def getPlots(startYear, endYear, initialSrc, firsDerSrc, secDerSrc, percEff):
     # Saving Plot 6 - Consumer Price
     consumCost = get_aConsumerCost(years, percSources)
     plt.plot(years, consumCost)
-    plt.xticks(list(range(startYear, endYear+1, 1)))
+    if endYear-StartYear < 10:
+        plt.xticks(list(range(startYear, endYear+1, 1)))
     plt.xlabel("Years")
     plt.ylabel("Cost (cents/kWh)")
     plt.title("Annual Consumer Costs")
