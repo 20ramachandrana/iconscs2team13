3ba74da83fc2ac525a077a969209d9aab6bcc0fd
20ramachandrana <44482134+20ramachandrana@users.noreply.github.com>
2021-04-24 14:52:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -24,16 +24,6 @@ def calc():
     else:
         return render_template("data.html", data = "")
     
-
-# the interface of the second calculator (autogenerating the number of fields needed) – 
-# defines the "get" request behavior to be loading the page from a template and the "post" request behavior to load a data page
-# will have a way to talk to helper.py to get the plotting functions and display the plots on the page
-@app.route('/calculatorver2', methods = ['GET', 'POST'])
-def calcver2():
-    if request.method == 'GET':
-        return render_template("calcver2.html")
-    else:
-        return render_template("data2.html", data = "")
     
 # runs our app using Flask
 if __name__ == "__main__":
